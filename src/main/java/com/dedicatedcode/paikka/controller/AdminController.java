2c730ffe41d2e2771383dbb6a0c1f7868e758669
Daniel Graf <daniel@dedicatedcode.com>
2026-02-20 05:22:34 +0100

    clenaup

--- src/main/java/com/dedicatedcode/paikka/controller/AdminController.java
+++ src/main/java/com/dedicatedcode/paikka/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.dedicatedcode.paikka.service.ReverseGeocodingService;
 import com.dedicatedcode.paikka.service.BoundaryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +18,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
+@ConditionalOnProperty(name = "paikka.import-mode", havingValue = "false", matchIfMissing = true)
 public class AdminController {
     
     private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
